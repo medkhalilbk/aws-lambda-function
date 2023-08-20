c99d18c6cfdd45db79aa006df2476d0da84ec659
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-08-20 05:17:14 +0100

    update for AWS

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const axios = require('axios')
 require('dotenv').config()
 
 
-const articles = [] ; 
-
-
+const articles = [] ;  
 async function fetchForexArticles() {
     try {
      const res =  await (axios.get('https://newsapi.org/v2/top-headlines?category=business&apiKey=96e1c501488b41f1b15d8938e6d1a990') )
-     console.log(res.data)
+     console.log(res.data.articles) 
     }
     catch(err){
         console.log(err)
@@ -16,14 +14,6 @@ async function fetchForexArticles() {
  
  }
 
-async function functionOne() {
-   try {
-    const test = await axios.get('https://coffeecart.trading/wp-json/wp/v2/posts') 
-    console.log(test)
-   } catch (error) {
-    console.log(process.env.TEST)
-   }
-
-}
+ 
 fetchForexArticles()
 
